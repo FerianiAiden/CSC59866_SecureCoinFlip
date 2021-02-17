d7039b191245783ca38bec2ea116eca43d373a66
jp183 <47368976+jp183@users.noreply.github.com>
2021-02-17 17:02:06 -0500

    Add files via upload

---
+++ main page/style.js
@@ -0,0 +1,14 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function allowNumbersOnly(e) {
+    var code = (e.which) ? e.which : e.keyCode;
+    if (code > 31 && (code < 48 || code > 57)) {
+        e.preventDefault();
+    }
+}
