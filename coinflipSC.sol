35b8ee65a32d04b3d421b941ab96d307f914787f
FerianiAiden <ferianiaiden@gmail.com>
2021-02-22 19:30:11 -0500

    Implemented VRF CL to generate randomness (testing needed)

--- coinflipSC.sol
+++ coinflipSC.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.8.1;
-
-    contract CoinFlip{
+import "https://raw.githubusercontent.com/smartcontractkit/chainlink/master/evm-contracts/src/v0.6/VRFConsumerBase.sol";
+    contract CoinFlip is VRFConsumerBase {
       
         uint256 public betAmount >= 1.0 finney;
         address public player;
@@ -14,6 +14,12 @@ pragma solidity 0.8.1;
 
         modifier onlyPlayer(){
         require(msg.sender == player);
+
+        /*/ for ChainLink vv /*/
+        bytes32 internal keyHash;
+        uint256 internal fee;
+        uint256 public randomResult;
+        
         }
 
         constructor(bytes32 _resultCommitment, bool _result, bytes32 _playersCommitment, address _player) 
@@ -24,6 +30,16 @@ pragma solidity 0.8.1;
             resultCommitment = _resultCommitment;
             player = _player;
             result = _result;
+
+
+              VRFConsumerBase(
+            0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
+            0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token
+        ) public
+    {
+        keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
+        fee = 0.1 * 10 ** 18; // 0.1 LINK
+    }
         }
 
 // Call the following function to fund the contract
@@ -82,15 +98,19 @@ pragma solidity 0.8.1;
             player = _player;
         }
 
+         /*/ RANDOM NUMBER GENERATION /*/
+
+
+         function GenRandomNum(uint256 userProvidedSeed) public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) > fee, "Not enough LINK - fill contract with faucet");
+        return requestRandomness(keyHash, fee, userProvidedSeed);
+
+            function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomResult = randomness;
+   
         }
-
-
-
-
-
-
-    
-
-
+        }
     }
+
+       
 
