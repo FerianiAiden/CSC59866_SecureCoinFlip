71326c5fc57fa7feebdea82d48b9ea7ed273ae72
FerianiAiden <ferianiaiden@gmail.com>
2021-02-24 00:16:48 -0500

    removed CL VRF

--- coinflipSC.sol
+++ coinflipSC.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.8.1;
-import "https://raw.githubusercontent.com/smartcontractkit/chainlink/master/evm-contracts/src/v0.6/VRFConsumerBase.sol";
-    contract CoinFlip is VRFConsumerBase {
+
+    contract CoinFlip {
       
         uint256 public betAmount >= 1.0 finney;
         address public player;
@@ -15,10 +15,7 @@ import "https://raw.githubusercontent.com/smartcontractkit/chainlink/master/evm-
         modifier onlyPlayer(){
         require(msg.sender == player);
 
-        /*/ for ChainLink vv /*/
-        bytes32 internal keyHash;
-        uint256 internal fee;
-        uint256 public randomResult;
+        
         
         }
 
@@ -31,15 +28,6 @@ import "https://raw.githubusercontent.com/smartcontractkit/chainlink/master/evm-
             player = _player;
             result = _result;
 
-
-              VRFConsumerBase(
-            0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
-            0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token
-        ) public
-    {
-        keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
-        fee = 0.1 * 10 ** 18; // 0.1 LINK
-    }
         }
 
 // Call the following function to fund the contract
@@ -98,18 +86,7 @@ import "https://raw.githubusercontent.com/smartcontractkit/chainlink/master/evm-
             player = _player;
         }
 
-         /*/ RANDOM NUMBER GENERATION /*/
-
-
-         function GenRandomNum(uint256 userProvidedSeed) public returns (bytes32 requestId) {
-        require(LINK.balanceOf(address(this)) > fee, "Not enough LINK - fill contract with faucet");
-        return requestRandomness(keyHash, fee, userProvidedSeed);
-
-            function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
-        randomResult = randomness;
-   
-        }
-        }
+      
     }
 
        
