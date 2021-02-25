0f50508fbe317a9998b5bc4ec68a256af16c2337
FerianiAiden <ferianiaiden@gmail.com>
2021-02-24 22:21:54 -0500

    added commitments for player choice and result of coinflip

--- coinflipSC.sol
+++ coinflipSC.sol
@@ -2,33 +2,14 @@ pragma solidity 0.8.1;
 
     contract CoinFlip {
       
-        uint256 public betAmount >= 1.0 finney;
+        uint256 public betAmount = 0.001 ether;
         address public player;
         bool public result;
-        bool public resultCommitment;
         bytes32 public playersCommitment;
-        bool public Pchoice;
-        bool public Pmade_choice = false;
+        bool public PlayerChoice;
+        bool public Player_Chose = false;
         bool public coinRevealed = false;
-        uint256 public expiration = 2**256-1; //Almost infinite
-
-        modifier onlyPlayer(){
-        require(msg.sender == player);
-
-        
-        
-        }
-
-        constructor(bytes32 _resultCommitment, bool _result, bytes32 _playersCommitment, address _player) 
-            public payable onlyPlayer{
-            require(msg.value == betAmount);
-            require(_player != address(0));
-            playersCommitment = _playersCommitment;
-            resultCommitment = _resultCommitment;
-            player = _player;
-            result = _result;
-
-        }
+        uint256 public expiration = 2**256-1; 
 
 // Call the following function to fund the contract
 
@@ -44,42 +25,54 @@ pragma solidity 0.8.1;
                 return address(this).balance;
             }
 
+modifier onlyPlayer(){
+    require(msg.sender == player);
+}
 
+     // Constructor used to intitialize a new game
+        constructor(bytes32 _resultCommitment, bytes32 _playersCommitment, address _player) 
+        public payable onlyPlayer{
+            require(msg.value >= betAmount);
+            require(_player != address(0));
 
+            playersCommitment = _playersCommitment;
+            resultCommitment = _resultCommitment;
+            player = _player;
+        }
 
 // Executed when player wants to make their choice via paying bet 
 // true = heads, false = tails
      
-        function  ChooseSide(bool _Pchoice) public payable onlyPlayer{
+        function  ChooseSide(bool _PlayerChoice) public payable onlyPlayer{
             require(msg.value == betAmount);
             require(!Pmade_choice);
-            Pchoice = _Pchoice;
-            Pmade_choice = true;
-            expiration =  now + 2 hours; 
+            PlayerChoice = _PlayerChoice;
+            Player_Chose = true;
+            expiration =  now + 1 hours; 
         }
-
-        function revealFlip(bool result, uint256 nonce) public {
-            require(Pmade_choice);
+// In this function we are revealing whether or not the player won. 
+// The hashed result of the coin must match its commitment, and similarly the hashed player choice must match their comittment
+        function revealFlip(bool result, bool PlayerChoice uint256 nonce) public {
+            require(Player_Chose);
             require(keccak256(abi.encodePacked(result, nonce)) == resultCommitment);
-            require(expiration > now);
+            require(keccak256(abi.encodePacked(PlayerChoice, nonce)) == playersCommitment);
+            require(expiration > now + 5 minutes);
 
-            //For a game to start:
+            //For a new game to start:
             Pmade_choice = false;
             coinRevealed = true;
             expiration = 2**256-1;
 
-            if(result == Pchoice){
+            if(result == PlayerChoice){
                 player.transfer(2*betAmount);
-            } else {
-                /*/ u lose hahahahahahahaha /*/
-            }
+            
         }
 
         function newGame(bytes42 _resultCommitment, address _player) public payable onlyPlayer {
             require(msg.value == betAmount);
             require(coinRevealed);
             require(_player != address(0));
-            require(!Pmade_choice);
+            require(!Player_Chose);
 
             coinRevealed = false;
             resultCommitment = _resultCommitment;
