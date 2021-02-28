a19470eb55d8158bef452bd143f956e27066eb6f
FerianiAiden <ferianiaiden@gmail.com>
2021-02-27 21:42:23 -0500

    Implemented DetermineWinner(), ForfeitGame(), newGame()

--- coinflipSC.sol
+++ coinflipSC.sol
@@ -4,7 +4,7 @@ import "https://raw.githubusercontent.com/smartcontractkit/chainlink/master/evm-
     contract CoinFlip is VRFConsumerBase {
       
         uint256 public betAmount;
-        address public player;  // players address
+        address payable public player;  // players address
         
         
         bool public playerChoice; // players guess(heads or tails)
@@ -16,6 +16,15 @@ import "https://raw.githubusercontent.com/smartcontractkit/chainlink/master/evm-
         bool public Player_Chose;
         bool public coinRevealed;
         uint256 public expiration; 
+        
+        // new variables bro
+        mapping (address => uint) balances; // Keeps track of the transactions made with this contract via player address
+        address[] _players; // this is used in ChooseSide - will add the address of the player that pays bet fee 
+        
+        
+     
+        
+       
         
         //for chainlink vrf
         bytes32 internal keyHash;
@@ -55,7 +64,7 @@ import "https://raw.githubusercontent.com/smartcontractkit/chainlink/master/evm-
     
 // Call the following function to fund the contract
 
-            mapping (address => uint) balances;
+            
             function fillContract() external payable {
                 if(msg.value < 1 ether) {
                     revert();
@@ -78,34 +87,58 @@ modifier onlyPlayer(){
 // true = heads, false = tails
      
         function  ChooseSide(bool _PlayerChoice) public payable onlyPlayer{
-            require(msg.value == betAmount);
+            require(msg.value >= betAmount);
             require(!Player_Chose);
+            betAmount = msg.value;
+            
             playerChoice = _PlayerChoice;
             Player_Chose = true;
             expiration =  now + 1 hours; 
+            _players.push(msg.sender);
         }
         
 // In this function we are revealing whether or not the player won. 
 // The hashed result of the coin must match its commitment, and similarly the hashed player choice must match their comittment
-        function revealFlip(uint256 _result, bool PlayerChoice, uint256 nonce) public {
+        function revealFlip(uint256 _result,  uint256 nonce) public {
             require(Player_Chose);
             //require(keccak256(abi.encodePacked(result, nonce)) == resultCommitment);
-            require(keccak256(abi.encodePacked(PlayerChoice, nonce)) == playerCommitment);
+            require(keccak256(abi.encodePacked(playerChoice, nonce)) == playerCommitment);
             require(expiration > now + 5 minutes);
-
+           
+            
+           
             //For a new game to start:
             Player_Chose = false;
             coinRevealed = true;
             expiration = 2**256-1;
-
-            // if(result == PlayerChoice){
-            //     
-            //     player.transfer(2*betAmount);
-            //     }
+            
+            
+}
+        /*/ NEW VVVV /*/
+        function determineWinner() public {
+            bytes1 temp1;
+            bytes1 temp2;
+            if (playerChoice = true){
+                temp1 = 0x01; 
+            } 
+            else {
+                temp1 = 0x0;
+            }
+            if (casinoChoice = true) {
+                temp2 = 0x01;
+            }
+            else {
+                temp2 = 0x0;
+            }
+            
+            if (temp1 ^ temp2 == 0){
+                player.transfer(2*betAmount);
+            }
+        
         }
-
-        function newGame(bytes32 _casinoCommitment, address _player) public payable onlyPlayer {
-            require(msg.value == betAmount);
+        /*/ NEW functions VVVVVV /*/ 
+        function newGame(bytes32 _casinoCommitment, address payable _player) public payable onlyPlayer {
+            require(msg.value >= betAmount);
             require(coinRevealed);
             require(_player != address(0));
             require(!Player_Chose);
@@ -114,7 +147,15 @@ modifier onlyPlayer(){
             casinoCommitment = _casinoCommitment;
             player = _player;
         }
-
-      
+        
+        function ForfeitGame() public onlyPlayer { //If the Casino refuses to reveal the coin result 
+            require(now > expiration);
+            require(Player_Chose);
+            //we may have to require that the random value has already been produced, 
+            player.transfer(betAmount);
+            Player_Chose = false;
+            
+            
+        }
         
     }
