3b8dcdd1e0b852613241502c2e8f84c86eed7628
MingHin-Cheung <33229513+MingHin-Cheung@users.noreply.github.com>
2021-03-01 18:41:30 -0500

    add trycatch

--- conflip_test.sol
+++ conflip_test.sol
@@ -7,20 +7,28 @@ contract confilpTest {
     CoinFlip v;
    
     function beforeEach() public {
-    // Create an instance of contract to be tested
-    v = new CoinFlip();
+        // Create an instance of contract to be tested
+        v = new CoinFlip();
     }
-    // test initialValue 
+    
+    // test initialValue = 0
     function initialValueShouldBe0() public returns (bool) {
         uint expected = 0;
         return Assert.equal(v.contractBalance(), expected, "initial contract balance is 0");
     }
-    // malicious user who tries to  fill contract with less than 1 milliether
-    function spendlessthan1milliether() public returns (bool) {
+    
+    // malicious user who tries to  fillc ontract with less than 1 milliether
+    function test_spendlessthan1milliether_trycatch() public  returns (bool){
         // fillcontract with 0 eth
-        v.fillContract();
+        try v.fillContract(){}
+        catch (bytes memory /*lowLevelData*/) {
+            // This is executed in case revert() was used
+            // or there was a failing assertion, division
+            // by zero, etc. inside getData.
+            Assert.ok(false, 'The contract balance is 0, you must send a minimum of 0.001 ETH to the contract, or it will revert');
+        }
+        
         // Transaction has been reverted by the EVM. Because the contractbalance is 0,you must send a minimum of 0.001 ETH to the contract, or it will revert .
     }
 
-    
 }
