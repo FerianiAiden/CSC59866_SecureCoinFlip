0e81512875f6fc0ef456bc66a163ead0f026d8aa
MingHin-Cheung <33229513+MingHin-Cheung@users.noreply.github.com>
2021-03-02 17:33:08 -0500

    Add files via upload

--- conflip_test.sol
+++ conflip_test.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.6.6;
 import "remix_tests.sol"; // this import is automatically injected by Remix.
 //import "remix_accounts.sol";
-import "../contracts/MyContract.sol";
+import "../contracts/coinflipSC.sol";
 
 contract confilpTest {
     CoinFlip v;
@@ -19,16 +19,15 @@ contract confilpTest {
     
     // malicious user who tries to  fillc ontract with less than 1 milliether
     function test_spendlessthan1milliether_trycatch() public  returns (bool){
-        // fillcontract with 0 eth
-        try v.fillContract(){}
+        // try to choose side with 0 eth
+        try v.ChooseSide(true){}
         catch (bytes memory /*lowLevelData*/) {
             // This is executed in case revert() was used
             // or there was a failing assertion, division
             // by zero, etc. inside getData.
             Assert.ok(false, 'The contract balance is 0, you must send a minimum of 0.001 ETH to the contract, or it will revert');
         }
-        
-        // Transaction has been reverted by the EVM. Because the contractbalance is 0,you must send a minimum of 0.001 ETH to the contract, or it will revert .
+
     }
 
 }
